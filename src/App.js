88f90fa0a6b651392bf141ca19ed0e98483e424d
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2019-09-08 22:51:15 +0900

    Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ fav }){
+  return <h3>I like {fav}</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
     </div>
   );
 }
